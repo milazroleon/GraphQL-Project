cd502ed343776971e96eb65d208919af7276268d
milazroleon <laurarodleo@unisabana.edu.co>
2025-11-01 11:13:30 -0500

    Add allowed origin for GraphQL project
    Added a new allowed origin for CORS configuration.

--- backend/src/main/java/backend/config/securityConfig.java
+++ backend/src/main/java/backend/config/securityConfig.java
@@ -27,6 +27,7 @@ public class securityConfig {
       "http://localhost:5174",
       "http://localhost:5175",
       "http://localhost:5176"
+      "https://graphqlproject-lake.vercel.app"
     ));
     cfg.addAllowedHeader("Content-Type");
     cfg.addAllowedHeader("Authorization");
